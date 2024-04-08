d0b84dcf73bfc565504a2c3d83ca8957aba8e0e7
jschwabe <mail@jschwabe.site>
2024-04-08 16:05:38 +0000

    add files for ex01
    Signed-off-by: jschwabe <mail@jschwabe.site>

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+# include <iostream>
+
+class Fixed {
+public:
+	Fixed();
+	Fixed(int const raw);
+	Fixed(float const raw);
+	Fixed(Fixed const& src);
+	~Fixed();
+
+	Fixed& operator=(Fixed const& rhs);
+	int    getRawBits() const;
+	void   setRawBits(int const raw);
+	float  toFloat() const;
+	int    toInt() const;
+
+private:
+	int              _value;
+	static const int _fractionalBits = 8;
+};
+
+std::ostream& operator<<(std::ostream& o, Fixed const& i);
